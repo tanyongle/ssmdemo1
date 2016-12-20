583516fd95e1f81b1eed2cb3a58e47f91975ab96
terrence <yongle_tan@163.com>
2016-12-20 11:21:23 +0800

    init project

--- ssmdemo1/src/main/java/com/ssm/web/BookController.java
+++ ssmdemo1/src/main/java/com/ssm/web/BookController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -13,7 +14,7 @@ public class BookController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-    @RequestMapping(value = "/helpme")
+    @RequestMapping(value = "/helpme", method = RequestMethod.GET)
     public String helpme() {
         return "helpme";
     }
